7d98c2f3e867a72bbf07869ad09b3d928ed27f75
Adhil Fouzi <adhilfouzi@gmail.com>
2024-06-01 12:50:27 +0530

    revenue screen loading

--- lib/view/course/revenue/revenue_screen.dart
+++ lib/view/course/revenue/revenue_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../view_model/course/transaction_controller.dart';
 import 'utils/date_filter.dart';
 import 'utils/total_revenue_section.dart';
@@ -11,7 +10,7 @@ class Revenue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TransactionController controller = Get.find();
+    final TransactionController controller = Get.find<TransactionController>();
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
 
@@ -24,40 +23,57 @@ class Revenue extends StatelessWidget {
             fontSize: 25,
           ),
         ),
-        centerTitle: true,
+        centerTitle: false,
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.fetchTransaction();
+              },
+              icon: const Icon(Icons.refresh))
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(
             horizontal: width * 0.05, vertical: height * 0.01),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Obx(() => TotalRevenueSection(
-                  revenueAsPerDay: controller.revenueAsPerDay,
-                  totalAmount: controller.totalAmount,
-                )),
-            SizedBox(height: height * 0.02),
-            Obx(() => DateFilter(
-                  startDate: controller.startDate,
-                  endDate: controller.endDate,
-                  onDateRangeSelected: (DateTimeRange? picked) {
-                    if (picked != null) {
-                      controller.updateDateRange(picked.start, picked.end);
-                    }
-                  },
-                )),
-            SizedBox(height: height * 0.02),
-            Expanded(
-              child: Obx(() {
-                final bookingsByDate = controller.bookingsByDate;
-                return bookingsByDate.isNotEmpty
-                    ? TransactionList(bookingsByDate: bookingsByDate)
-                    : const Center(
-                        child: Text("No Transactions Available"),
-                      );
-              }),
-            ),
-          ],
+        child: Obx(
+          () => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Observe total revenue and revenue per day
+              TotalRevenueSection(
+                revenueAsPerDay: controller.revenueAsPerDay,
+                totalAmount: controller.totalAmount,
+              ),
+              SizedBox(height: height * 0.02),
+              // Observe date filter
+              DateFilter(
+                startDate: controller.startDate,
+                endDate: controller.endDate,
+                onDateRangeSelected: (DateTimeRange? picked) {
+                  if (picked != null) {
+                    controller.updateDateRange(picked.start, picked.end);
+                  }
+                },
+              ),
+              SizedBox(height: height * 0.02),
+              // Observe bookings by date
+              Expanded(
+                child: controller.isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : SingleChildScrollView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        child: SizedBox(
+                          height: height * 0.6, // Adjust height as needed
+                          child: controller.bookingsByDate.isNotEmpty
+                              ? TransactionList(
+                                  bookingsByDate: controller.bookingsByDate)
+                              : const Center(
+                                  child: Text("No Transactions Available")),
+                        ),
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
